9aa5f1cb66f6e9277450cb7ac9659b1a161da2ff
Cody Nissan <cnih150@gmail.com>
2025-01-16 19:55:22 -0500

    Added LimeLight subsystem. Yay :D

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,7 +8,7 @@ import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
-import frc.robot.subsystems.LLVision;
+import frc.robot.subsystems.LimeLight;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -22,7 +22,8 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-  private final LLVision vision = new LLVision("Erriot", "Erriot2", "Homes3", "Jormamabngr");
+  private final LimeLight limelight = new LimeLight("limelight");
+  private final LimeLight limelight2 = new LimeLight("limelight2");
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
---
+++ src/main/java/frc/robot/subsystems/LimeLight.java
@@ -0,0 +1,43 @@
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+public class LimeLight extends SubsystemBase {
+
+    String name;
+    NetworkTable table;
+    NetworkTableEntry xOffsetEntry;
+    NetworkTableEntry yOffsetEntry;
+    NetworkTableEntry areaEntry;
+
+    public LimeLight(String name) {
+        this.name = name;
+        table = NetworkTableInstance.getDefault().getTable(name);
+        xOffsetEntry = table.getEntry("tx");
+        yOffsetEntry = table.getEntry("ty");
+        areaEntry = table.getEntry("ta");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getXOffset() {
+        return xOffsetEntry.getDouble(361.0);
+    }
+
+    public double getYOffset() {
+        return yOffsetEntry.getDouble(69.0);
+    }
+
+    public double getAreaOffset() {
+        return areaEntry.getDouble(101.0);
+    }
+    
+}
